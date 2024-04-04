cd6a3ddd5bc97ff656eacbe274e2f6f3d9fe5d59
OlgaRumRum <Olga.tyuren@yandex.ru>
2024-04-04 15:02:45 +0300

    Create clicks on the home screen buttons

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.example.playlistmaker
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        val searchButton = findViewById<Button>(R.id.search_button)
+
+
+        val searchButtonListener: View.OnClickListener = object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                Toast.makeText(this@MainActivity, "Кнопка Поиск", Toast.LENGTH_SHORT).show()
+            }
+        }
+        searchButton.setOnClickListener(searchButtonListener)
+
+
+        val libraryButton = findViewById<Button>(R.id.library_button)
+
+        libraryButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Кнопка Медиатека", Toast.LENGTH_SHORT).show()
+        }
+
+        val settingsButton = findViewById<Button>(R.id.settings_button)
+
+        settingsButton.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Кнопка Настройки", Toast.LENGTH_SHORT).show()
+        }
+
     }
 }
