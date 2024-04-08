7f5d39bd01914c6ee564f34bcd7db4d5cc3752e0
OlgaRumRum <Olga.tyuren@yandex.ru>
2024-04-08 10:22:34 +0300

    Making recommendations

--- app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SettingsActivity.kt
@@ -1,22 +1,19 @@
 package com.example.playlistmaker
 
-import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_settings)
 
-        val arrowBackButton = findViewById<Button>(R.id.arrow_back)
+        val toolbarArrowBack = findViewById<Toolbar>(R.id.toolbar_arrow_back)
 
-        arrowBackButton.setOnClickListener {
-
-            val displayIntent = Intent(this@SettingsActivity, MainActivity::class.java)
-            startActivity(displayIntent)
-
+        toolbarArrowBack.setNavigationOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
         }
+
     }
 }
