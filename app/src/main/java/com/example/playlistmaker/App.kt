86482078d587e9a8322ccb837e9a181687079be1
OlgaRumRum <Olga.tyuren@yandex.ru>
2024-07-21 09:26:55 +0300

    delete comments

--- app/src/main/java/com/example/playlistmaker/App.kt
+++ app/src/main/java/com/example/playlistmaker/App.kt
@@ -8,33 +8,6 @@ import androidx.appcompat.app.AppCompatDelegate
 const val PLAYLIST_MAKER_PREFERENCES = "playlist_maker_preferences"
 const val THEME_KEY = "key_for_the_theme"
 
-/*class App : Application() {
-
-var darkTheme = false
-    private set
-
-override fun onCreate() {
-    super.onCreate()
-    val sharedPrefs = getSharedPreferences(PLAYLIST_MAKER_PREFERENCES, MODE_PRIVATE)
-    darkTheme = sharedPrefs.getBoolean(THEME_KEY, false)
-    switchTheme(darkTheme)
-
-    Creator.initApplication(this)
-}
-
-fun switchTheme(darkThemeEnabled: Boolean) {
-    darkTheme = darkThemeEnabled
-    AppCompatDelegate.setDefaultNightMode(
-        if (darkThemeEnabled) {
-            AppCompatDelegate.MODE_NIGHT_YES
-        } else {
-            AppCompatDelegate.MODE_NIGHT_NO
-        }
-    )
-}
-}
-
- */
 
 class App : Application() {
 
@@ -45,11 +18,10 @@ class App : Application() {
         sharedPrefs = getSharedPreferences(
             PLAYLIST_MAKER_PREFERENCES,
             MODE_PRIVATE
-        ) // Инициализация sharedPreferences
+        )
         darkTheme = sharedPrefs.getBoolean(THEME_KEY, false)
         switchTheme(darkTheme)
 
-        // Инициализация других компонентов, если необходимо
         Creator.initApplication(this)
     }
 
